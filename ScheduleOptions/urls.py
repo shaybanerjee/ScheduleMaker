7cd631f316d558bb83456c752380792554233bb3
shaybanerjee <shayon@live.ca>
2017-06-14 23:28:52 -0400

    Started Setup

--- ScheduleOptions/urls.py
+++ ScheduleOptions/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+# imported to redirect root url to another urls
+from django.views.generic import RedirectView
+# imported so that django accepts static files for development
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^schedule/', include('schedule.urls')),
+    url(r'^$', RedirectView.as_view(url='/schedule/', permanent=True)),
 ]
+
+# this line allows us to use static files for our developement
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
